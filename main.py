7240a362222fd14a938dce160272b077047486a8
clorden1 <cpordenla@gmail.com>
2025-03-03 23:07:23 -0600

    added button labels (commented out styling options)

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+#import dependecies
 import tkinter as tk
+from tkinter import ttk
 
-#define window
+#define window 
 root = tk.Tk()
+root.geometry("700x400")
+#Title window
 root.title('Time to Plant')
+
+#Style widget window
+# style = ttk.Style()
+# style
+
+#Labels and buttons for region and date
+region_label = ttk.Label(text = 'Region').grid(column = 0, row = 0)
+region_button = ttk.Button()
+date_label = ttk.Label(text = 'Date').grid(column = 0, row = 3)
 
 #initalize window
 root.mainloop()
