7fe949359e5f8fe8c7b14c1e5ee076713a6cdc71
clorden1 <cpordenla@gmail.com>
2025-03-04 19:08:50 -0600

    added buttons for region and date selection

--- main.py
+++ main.py
@@ -14,8 +14,10 @@ root.title('Time to Plant')
 
 #Labels and buttons for region and date
 region_label = ttk.Label(text = 'Region').grid(column = 0, row = 0)
-region_button = ttk.Button()
+region_button = ttk.Button().grid(column = 1, row = 0)
+
 date_label = ttk.Label(text = 'Date').grid(column = 0, row = 3)
+date_button = ttk.Button().grid(column = 1, row = 3)
 
 #initalize window
 root.mainloop()
